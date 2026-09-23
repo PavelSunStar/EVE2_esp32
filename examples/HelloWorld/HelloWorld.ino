8cf60de62f4fb1cb559e891ac969fdaa0175ad2b
Pavel <57296565+PavelSunStar@users.noreply.github.com>
2026-09-23 06:53:37 +0300

    Add files via upload

--- examples/HelloWorld/HelloWorld.ino
+++ examples/HelloWorld/HelloWorld.ino
@@ -6,8 +6,8 @@ void setup(){
     Serial.begin(115200);
     while(!Serial){;}
 
-    GD.setPins(1, 3, 20, 5, 4, 32, 33);
-    if (!GD.init(MODE_640x480_57)) {
+    GD.setPins(1, 10, 12, 11, 13, -1, -1);
+    if (!GD.init(MODE640x480_57)) {
         Serial.println("EVE init FAILED");
         while (1) delay(100);
     }
